095a2c6ef5d6b5778fc7a745ccf77280c87061a1
Graciliana Kascher <eagraciliana@gmail.com>
2025-01-08 12:24:44 -0300

    [feat] scrapy off the category and subcategories

---
+++ src/data_collection/spiders/category_mercado_livre.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class CategoryMercadoLivreSpider(scrapy.Spider):
+    name = "category_mercado_livre"
+    allowed_domains = ["www.mercadolivre.com.br"]
+    start_urls = ["https://www.mercadolivre.com.br/categorias"]
+
+    def parse(self, response):
+        categories = response.css('div.categories__container')
+        for  category in categories:
+            category_name = category.css("h2.categories__title a::text").get(),
+            subcategories =  category.css("h3.categories__subtitle-title ::text").getall()
+            for subcategory in subcategories:
+                yield {
+                    "category" : category_name,
+                    "subcategory" : subcategory
+                }
+            
